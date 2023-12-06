1dc953cf34fbd0ea71954d0c0e78dfa3b12dbb44
0xTxbi <techiejossy@gmail.com>
2023-12-06 04:26:34 +0100

    feat: user entity, and auth controller

--- authentication-service/entities/User.ts
+++ authentication-service/entities/User.ts
@@ -1,13 +1,60 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from "typeorm";
+import * as bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
 
 @Entity()
 export class User {
 	@PrimaryGeneratedColumn()
-	id!: number;
+	id: number;
+
+	@Column({ unique: true })
+	username: string;
+
+	@Column({ unique: true })
+	email: string;
 
 	@Column()
-	username!: string;
+	salt: string;
 
 	@Column()
-	password!: string;
+	hashedPassword: string;
+
+	@BeforeInsert()
+	async hashCredentials() {
+		this.salt = await bcrypt.genSalt(16);
+		this.hashedPassword = await bcrypt.hash(
+			this.hashedPassword,
+			this.salt
+		);
+	}
+
+	async compareCredentials(
+		usernameOrEmail: string,
+		password: string
+	): Promise<boolean> {
+		if (!usernameOrEmail || !password) {
+			return false;
+		}
+
+		if (
+			usernameOrEmail !== this.username &&
+			usernameOrEmail !== this.email
+		) {
+			return false;
+		}
+
+		const validPassword = await bcrypt.compare(
+			password,
+			this.hashedPassword
+		);
+		return validPassword;
+	}
+
+	generateJWT() {
+		const payload = { userId: this.id, username: this.username };
+		const secret = process.env.JWT_SECRET || "your-secret-key";
+		const token = sign(payload, secret, { expiresIn: "1h" });
+
+		return token;
+	}
 }
---
+++ authentication-service/src/controllers/AuthController.ts
@@ -0,0 +1,121 @@
+import { JsonController, Post, Body } from "routing-controllers";
+import {
+	IsNotEmpty,
+	IsEmail,
+	MinLength,
+	validateOrReject,
+} from "class-validator";
+
+import { getCustomRepository } from "../../../shared/utils/getCustomRepository";
+import { User } from "../../entities/User";
+
+class CreateUserDto {
+	@IsNotEmpty({ message: "Username is required" })
+	username: string;
+
+	@IsNotEmpty({ message: "Email is required" })
+	@IsEmail({}, { message: "Invalid email format" })
+	email: string;
+
+	@IsNotEmpty({ message: "Password is required" })
+	@MinLength(8, {
+		message: "Password must be at least 8 characters long",
+	})
+	hashedPassword: string;
+}
+
+class LoginUserDto {
+	@IsNotEmpty({ message: "Username or email is required" })
+	usernameOrEmail: string;
+
+	@IsNotEmpty({ message: "Password is required" })
+	password: string;
+}
+
+@JsonController()
+export class AuthController {
+	private readonly userRepository = getCustomRepository(User);
+
+	@Post("/register")
+	async register(
+		@Body() userData: CreateUserDto
+	): Promise<{ token: string } | { error: string }> {
+		try {
+			// validate user input using class-validator
+			await validateOrReject(userData);
+
+			// check if the username or email is already registered
+			const existingUser = await this.userRepository.findOne({
+				where: [
+					{ username: userData.username },
+					{ email: userData.email },
+				],
+			});
+
+			if (existingUser) {
+				return {
+					error: "Username or email already exists.",
+				};
+			}
+
+			// create a new user
+			const newUser = this.userRepository.create(userData);
+
+			// save the newly created user to the database
+			await this.userRepository.save(newUser);
+
+			// generate a JWT token for the new user
+			const token = newUser.generateJWT();
+
+			// respond with the token
+			return { token };
+		} catch (errors) {
+			console.error("Registration failed:", errors);
+			return {
+				error: "Registration failed. Please check your input and try again.",
+			};
+		}
+	}
+
+	@Post("/login")
+	async login(
+		@Body() loginData: LoginUserDto
+	): Promise<{ token: string } | { error: string }> {
+		try {
+			// validate user input using class-validator
+			await validateOrReject(loginData);
+
+			// find the user by username or email
+			const user = await this.userRepository.findOne({
+				where: [
+					{ username: loginData.usernameOrEmail },
+					{ email: loginData.usernameOrEmail },
+				],
+			});
+
+			// throw an error if the user isn't found or the password is incorrect
+			if (
+				!user ||
+				!(await user.compareCredentials(
+					loginData.usernameOrEmail,
+					loginData.password
+				))
+			) {
+				return {
+					error: "Invalid username/email or password",
+				};
+			}
+
+			// generate a JWT token for the authenticated user
+			const token = user.generateJWT();
+
+			// respond with the token
+			return { token };
+		} catch (errors) {
+			console.error("Login failed:", errors);
+			return {
+				error: "Login failed. Please check your input and try again.",
+			};
+		}
+	}
+}
