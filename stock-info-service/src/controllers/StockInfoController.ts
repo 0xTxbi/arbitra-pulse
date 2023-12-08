91aec990e636e341c81782bfaf0d74ade3e61d00
0xTxbi <techiejossy@gmail.com>
2023-12-08 02:41:18 +0100

    feat: search stocks

--- stock-info-service/src/controllers/StockInfoController.ts
+++ stock-info-service/src/controllers/StockInfoController.ts
@@ -1,7 +1,8 @@
-import { JsonController, Get, Param } from "routing-controllers";
+import { JsonController, Get, Param, QueryParams } from "routing-controllers";
 import { StockInfo } from "../../entities/StockInfo";
 import axios from "axios";
 import * as dotenv from "dotenv";
+import { FilteredStock } from "../types";
 
 dotenv.config();
 
@@ -14,14 +15,14 @@ class ApiError extends Error {
 
 @JsonController("/stock")
 export class StockInfoController {
+	// retrieve stock info
 	@Get("/:stockSymbol")
 	async getStockInfo(
 		@Param("stockSymbol") stockSymbol: string
 	): Promise<StockInfo> {
 		try {
 			// retrieve stock information from the financial API
-			const apiUrl = `${process.env.EXT_FIN_API_URL}/tickers/${stockSymbol}?apiKey=${process.env.EXT_FIN_API_KEY}`;
-
+			const apiUrl = `${process.env.POL_FIN_API_URL}/tickers/${stockSymbol}?apiKey=${process.env.POL_FIN_API_KEY}`;
 			const response = await axios.get(apiUrl);
 
 			// map the response data to the StockInfo object
@@ -73,6 +74,42 @@ export class StockInfoController {
 			if (axios.isAxiosError(error)) {
 				throw new ApiError(
 					`Failed to retrieve stock information: ${error.message}`
+				);
+			}
+			throw error;
+		}
+	}
+
+	// search for stock
+	@Get("/search/:query")
+	async searchStocks(
+		@Param("query") query: string
+	): Promise<{ stocks: FilteredStock[] }> {
+		try {
+			// retrieve stock symbols by keyword
+			const searchApiUrl = `${process.env.TD_FIN_API_URL}/symbol_search?symbol=${query}&outputsize=5&apikey=${process.env.TD_FIN_API_KEY}`;
+			const response = await axios.get(searchApiUrl);
+
+			// filter out stocks whose exchange country isn't the United States
+			const filteredStocks = response.data.data.filter(
+				(stock: any) =>
+					stock.country === "United States"
+			);
+
+			// map the filtered stocks to an array of simplified stock objects
+			const stocks: FilteredStock[] = filteredStocks.map(
+				(stock: any) => ({
+					symbol: stock.symbol,
+					exchange: stock.exchange,
+					instrumentName: stock.instrument_name,
+				})
+			);
+
+			return { stocks };
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				throw new ApiError(
+					`Failed to perform stock search: ${error.message}`
 				);
 			}
 			throw error;
