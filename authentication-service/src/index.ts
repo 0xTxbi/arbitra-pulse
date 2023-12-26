1dd13a18bbd426d3e481c9d86ca0afa39ea5142e
0xTxbi <techiejossy@gmail.com>
2023-12-27 00:30:15 +0100

    auth: cors – set cors headers via middleware

--- authentication-service/src/index.ts
+++ authentication-service/src/index.ts
@@ -63,13 +63,13 @@ const app = createExpressServer({
 	},
 });
 
-// enable cors for all routes
+// cors config
 const corsOptions: CorsOptions = {
 	origin: (origin, callback) => {
 		const whitelist = [
 			"http://localhost:3000",
 			process.env.CLIENT_DOMAIN,
-		]; // replace with your allowed origins
+		];
 		if (whitelist.indexOf(origin) !== -1 || !origin) {
 			callback(null, true);
 		} else {
@@ -81,6 +81,13 @@ const corsOptions: CorsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+// add middleware to set cors headers
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", req.headers.origin);
+	res.header("Access-Control-Allow-Credentials", "true");
+	next();
+});
 
 // start the server
 const PORT = process.env.PORT || 3000;
