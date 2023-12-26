2acb1534a9b5bdb1115a4f58ca06b848d81efd4c
0xTxbi <techiejossy@gmail.com>
2023-12-27 00:49:10 +0100

    auth: – use default cors config

--- authentication-service/src/index.ts
+++ authentication-service/src/index.ts
@@ -1,37 +1,13 @@
 import "reflect-metadata";
-import express from "express";
-import { Action, useExpressServer } from "routing-controllers";
+import { Action, createExpressServer } from "routing-controllers";
 import { AuthController } from "./controllers/AuthController";
 
 import { verify } from "jsonwebtoken";
 import { getCustomRepository } from "../utils/getCustomRepository";
 import { User } from "arbitra-pulse-entities";
 
-import cors, { CorsOptions } from "cors";
-
 // set up express server
-const app = express();
-
-// cors config
-const corsOptions: CorsOptions = {
-	origin: (origin, callback) => {
-		const whitelist = [
-			"http://localhost:3000",
-			process.env.CLIENT_DOMAIN,
-		];
-		if (whitelist.indexOf(origin) !== -1 || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
-	},
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	credentials: true,
-};
-
-app.use(cors(corsOptions));
-
-useExpressServer({
+const app = createExpressServer({
 	controllers: [AuthController],
 	currentUserChecker: async (action: Action) => {
 		return new Promise(async (resolve, reject) => {
@@ -83,13 +59,7 @@ useExpressServer({
 			}
 		});
 	},
-});
-
-// add middleware to set cors headers
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", req.headers.origin);
-	res.header("Access-Control-Allow-Credentials", "true");
-	next();
+	cors: true,
 });
 
 // start the server
