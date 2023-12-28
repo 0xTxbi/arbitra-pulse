4635160154aaeec55e6d8040695b6fe4876f5ea2
0xTxbi <techiejossy@gmail.com>
2023-12-28 12:37:30 +0100

    feat: cors

--- stock-info-service/src/index.ts
+++ stock-info-service/src/index.ts
@@ -4,6 +4,21 @@ import { StockInfoController } from "./controllers/StockInfoController";
 
 const app = createExpressServer({
 	controllers: [StockInfoController],
+	cors: {
+		origin: (origin, callback) => {
+			const whitelist = [
+				"http://localhost:3000",
+				process.env.CLIENT_DOMAIN,
+			];
+			if (whitelist.indexOf(origin) !== -1 || !origin) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true,
+	},
 });
 
 app.listen(3001, () => {
