8af34e4227f76e26ae98c85002fb1bbfc1d5300a
0xTxbi <techiejossy@gmail.com>
2024-01-04 04:32:25 +0100

    news: cors

--- news-service/src/index.ts
+++ news-service/src/index.ts
@@ -4,6 +4,21 @@ import { NewsController } from "./controllers/NewsController";
 
 const app = createExpressServer({
 	controllers: [NewsController],
+	cors: {
+		origin: (origin, callback) => {
+			const whitelist = [
+				"http://localhost:3000",
+				process.env.CLIENT_DOMAIN,
+			];
+			if (whitelist.indexOf(origin) !== -1 || !origin) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true,
+	},
 });
 
 app.listen(3002, () => {
