d148605a6089569df7dfe992666d50af178af135
0xTxbi <techiejossy@gmail.com>
2024-01-07 15:02:44 +0100

    auth-service: –> dashboard: watchlist news

--- authentication-service/src/services/Dashboard.service.ts
+++ authentication-service/src/services/Dashboard.service.ts
@@ -1,5 +1,5 @@
-import { User } from "arbitra-pulse-entities";
-import { getUpcomingEarnings } from "../../utils/requests";
+import { User, Watchlist } from "arbitra-pulse-entities";
+import { getUpcomingEarnings, getWatchlistNews } from "../../utils/requests";
 import { Service } from "typedi";
 
 @Service()
@@ -9,12 +9,25 @@ export class DashboardService {
 			// log user
 			console.log(currentUser);
 			// fetch dashboard components data
+
+			// retrieve upcoming earnings
 			const upcomingEarnings = await getUpcomingEarnings();
+
+			// extract tickers from current user's watchlist
+			const userTickers = (currentUser.watchlist || []).map(
+				(item: Watchlist) => item.symbol
+			);
+
+			// retrieve watchlist news
+			const watchlistNews = await getWatchlistNews(
+				userTickers
+			);
 
 			// Return the aggregated data
 			return {
-				upcomingEarnings,
 				welcomeMessage: "Welcome to Your Dashboard!",
+				upcomingEarnings,
+				watchlistNews,
 			};
 		} catch (error) {
 			// handle and log errors
