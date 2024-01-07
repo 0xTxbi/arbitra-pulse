d5919eb7e0658c543cbc9e7b8968aafcaa424e4c
0xTxbi <techiejossy@gmail.com>
2024-01-07 15:01:33 +0100

    watchlist news request

--- authentication-service/utils/requests.ts
+++ authentication-service/utils/requests.ts
@@ -6,6 +6,7 @@ interface UpcomingEarnings {
 	ticker: string;
 }
 
+// retrieve upcoming earnings
 export async function getUpcomingEarnings(): Promise<UpcomingEarnings[]> {
 	try {
 		const response = await axios.get(
@@ -18,6 +19,79 @@ export async function getUpcomingEarnings(): Promise<UpcomingEarnings[]> {
 		return upcomingEarnings;
 	} catch (error) {
 		console.error("Failed to fetch upcoming earnings");
+
+		// robust error handling
+		if (error.response) {
+			// triggers when the request was made and the server responded with a status code that falls out of the range of 2xx
+			console.error(
+				`Response data: ${JSON.stringify(
+					error.response.data
+				)}`
+			);
+			console.error(
+				`Response status: ${error.response.status}`
+			);
+		} else if (error.request) {
+			// triggers when the request was made but no response was received
+			console.error(`Request: ${error.request}`);
+		} else {
+			// triggers when something happened in setting up the request that triggered an error
+			console.error(`Error: ${error.message}`);
+		}
+		return null;
+	}
+}
+
+// retrieve watchlist news
+export async function getWatchlistNews(tickers: string[]): Promise<any> {
+	try {
+		// initialise array to hold news
+		let watchlistNews = [];
+
+		for (const ticker of tickers) {
+			const newsForTicker = await getTickerNews(ticker);
+
+			// as the result is an array, push into the watchlistNews array
+			watchlistNews.push(newsForTicker[0]);
+		}
+
+		return watchlistNews;
+	} catch (error) {
+		console.error("Failed to fetch news from your watchlist");
+
+		// robust error handling
+		if (error.response) {
+			// triggers when the request was made and the server responded with a status code that falls out of the range of 2xx
+			console.error(
+				`Response data: ${JSON.stringify(
+					error.response.data
+				)}`
+			);
+			console.error(
+				`Response status: ${error.response.status}`
+			);
+		} else if (error.request) {
+			// triggers when the request was made but no response was received
+			console.error(`Request: ${error.request}`);
+		} else {
+			// triggers when something happened in setting up the request that triggered an error
+			console.error(`Error: ${error.message}`);
+		}
+		return null;
+	}
+}
+
+// retrieve news about a ticker
+export async function getTickerNews(ticker: string): Promise<any[]> {
+	try {
+		const response = await axios.get(
+			`https://arbitra-pulse-news.fly.dev/news/${ticker}`
+		);
+		const tickerNews = response.data;
+
+		return tickerNews;
+	} catch (error) {
+		console.error("Failed to fetch ticker news");
 
 		// robust error handling
 		if (error.response) {
